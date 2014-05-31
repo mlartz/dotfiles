6fe5442f9875a14fc1cd67390ddd9b59afcea4c9
Michael L. Artz <mlartz@gmail.com>
2014-05-30 21:53:32 -0400

    Add Dash support to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -11,6 +13,7 @@
 (defvar my-packages '(auto-complete
                       cider clojure-mode clojure-test-mode
                       color-theme-solarized
+                      dash-at-point
                       ido ido-ubiquitous
                       js2-mode json-mode
                       magit
